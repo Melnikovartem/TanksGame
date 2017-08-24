8aa4bd94dcb4086570f05f99dc02809ad0ce42c4
Artem Melnikov <melnikovam10@gmail.com>
2017-08-24 09:20:36 +0300

    Changed roctbb code + fixed bag with movement

--- game_module/game.py
+++ game_module/game.py
@@ -54,10 +54,10 @@ def new_battle(room_number):
     ##print("")
 
     #clear current state
-    c.execute("DELETE FROM statistics WHERE room = " + str(room))
-    c.execute("DELETE FROM actions WHERE room = " + str(room))
-    c.execute("DELETE FROM game WHERE room = " + str(room))
-    c.execute("DELETE FROM coins  WHERE room = " + str(room))
+    c.execute("DELETE FROM statistics WHERE room = " + room)
+    c.execute("DELETE FROM actions WHERE room = " + room)
+    c.execute("DELETE FROM game WHERE room = " + room)
+    c.execute("DELETE FROM coins  WHERE room = " + room)
 
 
     #make map
@@ -118,7 +118,7 @@ def new_battle(room_number):
         c.execute("INSERT INTO game (key,x,y,life,room) VALUES (?,?,?,?,?)", [player,x,y, str(health[player]), room])
     c.execute("UPDATE settings SET value = ? WHERE param = ?", ["running", "game_state"])
 
-    #coins
+    #generating 10 coins
     for i in range(10):
         x = random.randint(0, int(settings["width"]) - 1)
         y = random.randint(0, int(settings["height"]) - 1)
@@ -140,7 +140,7 @@ def new_battle(room_number):
             print("current tick:"+str(ticks)+" in room:" + room)
         if (ticks>int(settings['stop_ticks']) or lifeplayers<int(settings['game_stop'])):
             break
-        #chices - dict with choices of all players
+        #choices - dict with choices of all players
         choices = dict()
         ticks += 1
 
@@ -158,9 +158,7 @@ def new_battle(room_number):
 
         for player in players:
             choices[player] = ""
-
-            if player in banlist:
-                continue
+            
             try:
                 c.execute("SELECT code FROM players WHERE key = ?", [player])
                 code = c.fetchone()
@@ -169,184 +167,86 @@ def new_battle(room_number):
                 output_file.close()
                 module = imp.import_module("bots." + player)
                 makeChoice = getattr(module, "make_choice")
-##                print("Now running:" +player+" ("+names[player]+")")
                 if len(historyMap)==1:
                     choices[player] = makeChoice(int(coords[player]["x"]), int(coords[player]["y"]), historyMap); #тут выбор
                 else:
                     choices[player] = makeChoice(int(coords[player]["x"]), int(coords[player]["y"]), historyMap);  # тут выбор
             except Exception as e:
-##                print(player+" ("+names[player]+") has crashed :( :"+str(e))
-                history[player].append("crash")
                 choices[player] = "crash"
                 crashes[player]+=1
-                c.execute("INSERT INTO actions (key, value, room) VALUES (?, ?, ?)", [player, choices[player], room])
-                c.execute(
-                    "UPDATE statistics SET crashes = " + str(crashes[player]) + " WHERE key = ?",
-                    [player])
-                c.execute(
-                    "UPDATE statistics SET lastCrash = ? WHERE key = ?",
-                    [str(e), player])
-        conn.commit()
-        #print(historyMap)
+                c.execute("UPDATE statistics SET crashes = ? WHERE key = ?",[crashes[player], player])
+                c.execute("UPDATE statistics SET lastCrash = ? WHERE key = ?", [str(e), player])
         #Analize what each user does
         for player in players:
             x_now = coords[player]["x"]
             y_now = coords[player]["y"]
-
-            if choices[player]=="go_up":
-                steps[player]+=1
-                if int(y_now) > 0 and mainMap[x_now][y_now - 1] in ('.', '@'):
-                    if mainMap[x_now][y_now - 1] == '@':
-                        coins[player] += 1
-                    mainMap[x_now][y_now] = '.'
-                    healthMap[x_now][y_now] = 0
-                    coords[player]["y"] -= 1
-
-
-            if choices[player] == "go_down":
-                steps[player]+=1
-                if int(y_now) < int(settings["height"]) - 1 and mainMap[x_now][y_now + 1] in ('.', '@'):
-                    if mainMap[x_now][y_now + 1] == '@':
-                        coins[player] += 1
-                    mainMap[x_now][y_now] = '.'
-                    healthMap[x_now][y_now] = 0
-                    coords[player]["y"] += 1
-
-
-            if choices[player] == "go_left":
-                steps[player]+=1
-                if int(x_now) > 0 and mainMap[x_now - 1][y_now] in ('.', '@'):
-                    if mainMap[x_now - 1][y_now] == '@':
-                        coins[player] += 1
-                    mainMap[x_now][y_now] = '.'
-                    healthMap[x_now][y_now] = 0
-                    coords[player]["x"] -= 1
-
-
-            if choices[player] == "go_right":
-                steps[player] += 1
-                if int(x_now) < int(settings["height"]) - 1 and mainMap[x_now + 1][y_now] in ('.', '@'):
-                    if mainMap[x_now + 1][y_now] == '@':
-                        coins[player] += 1
-                    mainMap[x_now][y_now] = '.'
-                    healthMap[x_now][y_now] = 0
-                    coords[player]["x"] -= 1
-                    
-            #make for each movement    
-            if choices[player][:3] == "go_":
-                    y_now = coords[player]["y"]
-                    x_now = coords[player]["x"]
-                    mainMap[x_now][y_now] = player
-                    healthMap[x_now][y_now] = health[player]
-                    c.execute("UPDATE game SET y = " + str(coords[player]["y"]) + " WHERE key = ?", [player])
-                    c.execute(
-                        "DELETE FROM coins WHERE x = ? AND y = ? AND room = ?",
-                        [coords[player]["x"], coords[player]["y"], room])
-                
-
-
-            if choices[player]!="error":
-                c.execute("INSERT INTO actions (key, value, room) VALUES (?, ?, ?)", [player, choices[player], room])
-                history[player].append(choices[player])
-            else:
-##                print(player+" ("+names[player]+") sent incorrect command: "+str(choices[player]))
-                errors[player] += 1
-                history[player].append("error")
-            #db record
-        for player in players:
-            if player in banlist:
-                continue
-            px = coords[player]["x"]
-            py = coords[player]["y"]
-            if choices[player] == "fire_up":
-                shots[player] += 1
-                for y in range(py-1, -1, -1):
-                    if mainMap[px][y] == '#':
-                        break;
-                    if mainMap[px][y] not in ('.', '@'):
-                        hit_player = mainMap[px][y]
-
-                        health[hit_player]-=1
-                        healthMap[px][y] -= 1
-
-                        kills[player]+=1
-
-##                      print(player + " (" + str(health[player]) + ") hits " + str(hit_player) + " (" + str(
-##                              health[hit_player]) + ")" + " [" + str(px) + " ," + str(py) + "] -> [" + str(px) + ", " + str(
-##                              y) + "] " + choices[player])
-                        c.execute("UPDATE game SET life = " + str(health[hit_player]) + " WHERE key = ?", [hit_player])
-                        break
-            if choices[player] == "fire_down":
-                shots[player] += 1
-                for y in range(py+1, int(settings["height"])):
-                    if mainMap[px][y] == '#':
-                        break
-                    if mainMap[px][y] not in ('.', '@'):
-                        hit_player = mainMap[px][y]
-
-                        health[hit_player] -= 1
-                        healthMap[px][y] -= 1
-
-                        kills[player] += 1
-
-##                        print(player + " ("+str(health[player])+") hits " + str(hit_player) + " (" + str(
-##                              health[hit_player]) + ")" + " [" + str(px) + " ," + str(py) + "] -> ["+str(px)+", "+str(y)+"] " + choices[player])
-
-
-
-                        c.execute("UPDATE game SET life = " + str(health[hit_player]) + " WHERE key = ?", [hit_player])
-                        break
-            if choices[player] == "fire_left":
-                shots[player] += 1
-                for x in range(px-1, -1, -1):
-                    if mainMap[x][py] == '#':
-                        break
-                    if mainMap[x][py] not in ('.', '@'):
-                        hit_player = mainMap[x][py]
-
-                        health[hit_player] -= 1
-                        healthMap[x][py] -= 1
-
-                        kills[player] += 1
-
-##                        print(player + " (" + str(health[player]) + ") hits " + str(hit_player) + " (" + str(
-##                            health[hit_player]) + ")" + " [" + str(px) + " ," + str(py) + "] -> [" + str(x) + ", " + str(
-##                            py) + "] " + choices[player])
-
-
-
-                        c.execute("UPDATE game SET life = " + str(health[hit_player]) + " WHERE key = ?", [hit_player])
-                        break
-            if choices[player] == "fire_right":
-                shots[player] += 1
-                for x in range(px+1, int(settings["width"])):
-                    if mainMap[x][py] == '#':
-                        break
-                    if mainMap[x][py] not in ('.', '@'):
-                        hit_player = mainMap[x][py]
-
-                        health[hit_player] -= 1
-                        healthMap[x][py] -= 1
-
-                        kills[player] += 1
-
-##                        print(player + " (" + str(health[player]) + ") hits " + str(hit_player) + " (" + str(
-##                            health[hit_player]) + ")" + " [" + str(px) + " ," + str(py) + "] -> [" + str(x) + ", " + str(
-##                            py) + "] " + choices[player])
-
-
-
-
-                        c.execute("UPDATE game SET life = " + str(health[hit_player]) + " WHERE key = ?", [hit_player])
-                        break
             
-            if choices[player][:5] == "fire_":
-                c.execute(
-                    "UPDATE statistics SET kills = " + str(kills[player]) + " WHERE key = ?",
-                    [player])
-                #print(player + " sent "+choices[player])
-
-                # db record
+            #bot didn't crash but the command was bad
+            #should change it later
+            if choices[player] not in ("go_up","fire_up","go_down","fire_down","go_right","fire_right","go_left","fire_left","crash"):
+                errors[player] += 1
+                choices[player] = "error"
+            
+            history[player].append(choices[player])
+            c.execute("INSERT INTO actions (key, value, room) VALUES (?, ?, ?)", [player, choices[player], room])
+            #not error
+            if choices[player] in ("error", "crash"):
+                pass
+            #We can say that player wants to go somewere        
+            elif choices[player][:3] == "go_":
+                steps[player] += 1
+                    y_new, x_new = y_now, x_now
+                    direction = choices[3:]
+                    if direction == "up":
+                        y_new -= 1
+                    elif direction == "down":
+                        y_new += 1
+                    elif direction == "left":
+                        x_new -= 1
+                    elif direction == "right":
+                        x_new += 1
+                    # if the movement happens
+                    if x_new >= 0 and x _new < settings["width"] - 1 and y_new >= 0 and y_new < settings["height"] and mainMap[x_new][y_new] in ('.', '@'):
+                        if mainMap[x_new][y_new] == "@":
+                            coins[player] += 1
+                        c.execute("DELETE FROM coins WHERE x = ? AND y = ? AND room = ?", x_new, y_new, room])
+                    mainMap[x_now][y_now] = "."
+                    mainMap[x_new][x_new] = player
+                    healthMap[x_now][y_now] = 0
+                    healthMap[x_new][y_new] = health[player]
+                    coords[player]["x"], coords[player]["y"] = x_new, y_new
+                    c.execute("UPDATE game SET x = ? and y = ? WHERE key = ?", [x_new, y_new, player])
+            #player wants to fire
+            elif choices[player][:5] == "fire_":
+                shots[player] += 1
+                direction = chioces[plyer][5:]
+                step_x = -1
+                step_y = -1
+                to_x = now_x
+                to_y = now_y
+                if direction == "up":
+                   to_y = -1
+                elif direction == "down":
+                   to_y = settings["height"]
+                   step_y = 1
+                elif direction == "left":
+                   to_x = -1
+                elif direction == "right":                   
+                   to_x = settings["width"]
+                   step_x = 1
+                for x_change in range(now_x+1, to_x , step_x):
+                    for y_change in range(now_y+1, to_y , step_y):
+                        if mainMap[x_change][y_change] == '#':
+                            break
+                        elif mainMap[x_change][y_change] not in ('.', '@'):
+                            hit_player = mainMap[x_change][y_change]
+                            health[hit_player] -= 1
+                            healthMap[x_change][y_change] -= 1
+                            kills[player] += 1
+                            c.execute("UPDATE game SET life = ? WHERE key = ?", [health[hit_player], hit_player])
+                            c.execute("UPDATE statistics SET kills = ?  WHERE key = ?", [kills[player], player])
+                            break
+            #all comands were checked
 
             if int(health[player])>0:
                 c.execute(
@@ -365,31 +265,29 @@ def new_battle(room_number):
                     "UPDATE statistics SET errors = " + str(errors[player]) + " WHERE key = ?",
                     [player])
 
-
-            conn.commit()
-
         remove_list = []
         for hit_player in players:
-##            print(hit_player+" "+str(health[hit_player])+" - check")
             if health[hit_player] <= 0:
                 mainMap[coords[hit_player]['x']][coords[hit_player]['y']] = '.'
                 healthMap[coords[hit_player]['x']][coords[hit_player]['y']] = 0
                 health[hit_player] = 0
                 lifeplayers -= 1
-##                print(hit_player + " is dead!")
                 remove_list.append(hit_player)
         for p in remove_list:
-##            print(p + " - try to remove")
-##            print(players)
             players.remove(p)
-##            print(players)
+        #db record
         conn.commit()
+        #tick ends
         time.sleep(1.2)
-
+    #after game
     c.execute("UPDATE settings SET value = ? WHERE param = ?", ["stop", "game_state"])
     c.execute("UPDATE players SET room = -1 WHERE room = " + room)
     conn.commit()
     return settings
+
+
+
+
 if __name__ == "__main__":
     print("clear-clear all players hist rooms \nelse-start a game in test(0) room")
     x = input()
